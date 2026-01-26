0e4f5340300f0e252e9e17bd8f7e45d36cea7c39
Sanmith Ravinarayan Rao <rao.sanmith2005@gmail.com>
2026-01-26 08:50:44 +0530

    Update optimizer.m

--- MPC/optimizer.m
+++ MPC/optimizer.m
@@ -9,19 +9,8 @@ for i=1:n1
     else
         kk=kk+0;
     end
-    %%
-    % 
-    %   for x = 1:10
-    %       disp(x)
-    %   end
-    % 
+    
 end
-%%
-% 
-%   for x = 1:10
-%       disp(x)
-%   end
-% 
 if (kk==0) 
     return; 
 end
@@ -32,9 +21,6 @@ x_ini=zeros(n,m);
 lambda=x_ini;
 al=10;
 for km=1:38
-    %find the elements in the solution vector one by one
-    % km could be larger if the Lagranger multiplier has a slow
-    % convergence rate.
     lambda_p=lambda;
     for i=1:n
         w= H(i,:)*lambda-H(i,i)*lambda(i,1);
